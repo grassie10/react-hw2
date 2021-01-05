83b3b6e7a67c54481794b6f90af154baaf09ca06
Grace Liu <gxliu1@gmail.com>
2021-01-05 15:37:25 -0600

    completed section1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import notes from "../notes";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
 
 function App() {
   return (
     <div>
-    
+      <Header></Header>
+      <Note></Note>
+      <Footer></Footer>
     </div>
   );
 }
