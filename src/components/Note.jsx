70de3fcc780e6b0ac7af49902926fcb9a8de83cb
Grace Liu <gxliu1@gmail.com>
2021-01-06 15:52:23 -0600

    completed section 2

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,9 +3,15 @@ import notes from "../notes";
 
 function Note() {
   return (
-    <div class="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+    <div>
+      {notes.map(function(note) {
+        return(
+          <div class="note">
+            <h1>{note.title}</h1>
+            <p>{note.content}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
